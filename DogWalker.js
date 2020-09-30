ed17441e1d14461fbe5124c04f40e11370eda481
Josh <jolumese@live.com>
2020-09-30 12:32:36 -0400

    DogWalkerFixed
    Fixed DogWalker without sort() function

--- DogWalker.js
+++ DogWalker.js
@@ -1,17 +1,26 @@
 class DogWalker {
     numberOfWalksCalculator(dogSizes){
-    	let walks = 0;
-  		let dogSort = dogSizes.sort();
-  		while (dogSort.length != 0) {
-    		if (dogSort[0] == dogSort[1]) {
-    			walks += 1;
-	      		dogSort.splice(0, 2);
-	      } else {
-	          	walks += 1;
-	          	dogSort.splice(0, 1);
+    	let temp = [];
+	 	let walks = 0;
+	  	let searcher = 0;
+	 	temp.push(dogSizes.shift());
+	  	while (dogSizes.length != 0) {
+		    if (searcher > dogSizes.length - 1) {
+		        walks += 1;
+		        searcher = 0;
+		        temp.pop();
+		        temp.push(dogSizes.shift());
+	      } else if (dogSizes[searcher] != temp[0]) {
+	          searcher += 1;
+	      } else if (dogSizes[searcher] == temp[0]) {
+	          walks += 1;
+	          dogSizes.splice(dogSizes.indexOf(dogSizes[searcher]), 1)
+	          searcher = 0;
+	          temp.pop();
+	          temp.push(dogSizes.shift());
 	        }
-    	}
-    	return walks
+	    }
+    return walks + temp.length;
     }
 }
 
