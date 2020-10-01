b33ab335527b64613410f3a04dc28d83ee79fcc0
Josh <jolumese@live.com>
2020-10-01 02:01:37 -0400

    DogWalkerFixed2
    Used map instead of array

--- DogWalker.js
+++ DogWalker.js
@@ -1,27 +1,20 @@
 class DogWalker {
     numberOfWalksCalculator(dogSizes){
-    	let temp = [];
-	 	let walks = 0;
-	  	let searcher = 0;
-	 	temp.push(dogSizes.shift());
-	  	while (dogSizes.length != 0) {
-		    if (searcher > dogSizes.length - 1) {
-		        walks += 1;
-		        searcher = 0;
-		        temp.pop();
-		        temp.push(dogSizes.shift());
-	      } else if (dogSizes[searcher] != temp[0]) {
-	          searcher += 1;
-	      } else if (dogSizes[searcher] == temp[0]) {
-	          walks += 1;
-	          dogSizes.splice(dogSizes.indexOf(dogSizes[searcher]), 1)
-	          searcher = 0;
-	          temp.pop();
-	          temp.push(dogSizes.shift());
-	        }
-	    }
-    return walks + temp.length;
-    }
+    	let dict = {};
+		let walks = 0
+	  	for (let key = 0; key < dogSizes.length; key++) {
+	  		if (dict.hasOwnProperty(dogSizes[key]) && dict[dogSizes[key]] != 0) {
+		        dict[dogSizes[key]] = 0;
+		        walks += 1
+	      	} else {
+		        dict[dogSizes[key]] = 1;
+		    }
+		}
+		for (let key in dict) {
+			walks += dict[Number(key)]
+		}
+	return walks;
+	}
 }
 
 module.exports = DogWalker;
